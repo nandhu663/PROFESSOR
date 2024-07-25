712331cf1be71037e4ad4af28b31e20150aca58d
MrC《》VENOM <68112220+nandhu663@users.noreply.github.com>
2024-07-25 16:51:24 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/nandhunair1/PROFESSOR-BOT.git /PROFESSORBOT
+  git clone https://github.com/nandhu663/PROFESSOR.git /PROFESSORBOT
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /PROFESSORBOT
